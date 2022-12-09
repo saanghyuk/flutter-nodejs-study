c57a883af85be6fdadece94d3446fa3a103e3795
saanghyuk <saanghyuk@gmail.com>
2022-12-09 22:51:20 +0900

    Review

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
-//
 import 'package:flutter/material.dart';
 import 'package:flutterstudy/components/myWidget.dart' as My;
+import 'package:flutterstudy/pages/detailPage.dart';
 
 
 void main() => runApp(MyRouter());
@@ -18,185 +18,165 @@ class MyRouter extends StatelessWidget {
 }
 
 class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
+
+  MainPage({Key? key}) : super(key: key);
+
+  List<Map<String, dynamic>> _data = [
+    {
+      "imgPath": "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg",
+      "name": "A",
+      "isActive": true
+    },
+    {
+      "imgPath": "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg",
+      "name": "B",
+      "isActive": false
+    },
+    {
+      "imgPath": "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg",
+      "name": "B",
+      "isActive": false
+    }, {
+      "imgPath": "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg",
+      "name": "B",
+      "isActive": false
+    }, {
+      "imgPath": "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg",
+      "name": "B",
+      "isActive": false
+    }
+  ];
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       drawer: Drawer(),
       appBar: AppBar(
-        backgroundColor: Colors.green.shade500,
-        title: Text("Connections"),
-        centerTitle: true,
-        actions: [
-        IconButton(
-            onPressed: (){},
-            icon: Icon(Icons.settings)
-          )
-        ]
+          backgroundColor: Colors.green.shade500,
+          title: Text("Connections"),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.settings)
+            )
+          ]
       ),
-      body: Container(
-        color: Colors.yellow,
-        // Column - 세로는 부모의 Maximum - 즉 전체 화면의 세로 사이즈 쓰는 중. 가로는 자식 중 가장 큰놈
-        // 현재 Column의 사이즈: 가로는 딱 자식 요소 만큼만인 상태, 세로는 전체
-        child: Column(
-          children: <int>[1, 2, 3].map<Widget>(
-              (int i) => Row(
-                children: <Map<String, dynamic>>[
-                  {
-                    "imgPath": " ",
-                    "name": "",
-                    "isActive": true
-                  },
-                  {
-                    "imgPath": " ",
-                    "name": "",
-                    "isActive": false
-                  }
-                ].map<Widget>(
-                    (Map<String, dynamic> m) => My.MyWidget(
-                      imgPath: m['imgPath'],
-                      name: m['name'],
-                      isActive: m['isActive']
-                    )
-                ).toList(),
-              )
-          ).toList(),
-          // mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          // children: [
-          //   Container(
-          //     // width: MediaQuery.of(context).size.width * 0.5,
-          //     color: Colors.red,
-          //     child: Row(
-          //       children: [
-          //         My.MyWidget(imgPath: "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg", name: "name1", isActive: true),
-          //         // this._myWidget(context: context, imgPath: "https://cdn.pixabay.com/photo/2022/09/03/10/52/northern-gannets-7429282_1280.jpg", name: "name2", isActive: false),
-          //         My.MyWidget(
-          //             imgPath: "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg",
-          //             name: "Hi",
-          //             isActive: true
-          //         )
-          //       ],
-          //
-          //     ),
-          //   ),
-          //
-          // ],
-        )
-      )
+
+      // Recycler View를 갖고 해보자.
+      // 굉장히 편리한 Widget이 있다.
+      body: GridView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+
+        // scrollDirection: Axis.horizontal,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          // GridView의 MainAxis가 새로이다. ScrollDirection이 Axis.vertical이다. GridView의 옵션 보면 된다.
+          // crossAxis면 반대니깐 가로이다.
+          crossAxisCount: 2,
+          // main이 현재 Vertical. 가로 줄끼리의 간격.
+          // 영역을 차지하고 있는것은 여전히 정사각형이 맞다.
+          mainAxisSpacing: 10.0,
+          // crossAxis는 현재 세로간의 간격을  의미한다.
+          crossAxisSpacing: 10.0,
+          // 바깥쪽 부분도 여백이 필요하다.
+        ),
+
+
+        itemCount: this._data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: (){
+              print(index);
+              Navigator.of(context).push<dynamic>(
+                // MaterialPageRoute도 T를 받는데 다이나믹이 들어가고 생략되어 있던 것.
+                MaterialPageRoute<dynamic>(builder: (BuildContext _){
+                  return DetailPage();
+                })
+              );
+      
+            },
+            child: My.MyWidget(
+                imgPath: this._data[index]['imgPath'],
+                name: this._data[index]['name'],
+                isActive: this._data[index]['isActive']
+            ),
+          );
+        },
+        // GridView를 사용했다면, 리스트에 하나씩 넣거나, 반복문 통해서 써야 하는데, 직접 넣지는 않겠지.
+        // 반복문도 불편하다. 몇번째꺼 선택했는지 알아낼때가 굉장히 불편하다. indexOf로 찾고 난리쳐야 한다.
+        // 그걸 준비해놓은 GridView가 있다. 아래처럼 정적으로 넣을 일은 없다.
+        // GridView.builder, count, custom, extent
+        // children : [
+        //   My.MyWidget(imgPath: '', name: 'A', isActive: true),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: false),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: false),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: false),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: true),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: false),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: true),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: true),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: false),
+        //   My.MyWidget(imgPath: '', name: 'B', isActive: false),
+        // ]
+      ),
+
+      // 이전 Body전체 주석
+      // Scroll 내릴 수 있도록
+      // body: SingleChildScrollView(
+      //   // 조건에 따라서 스크롤 하지 못하도록 하거나 그러고 싶을 때. 예를 들어, 회원만 스크롤
+      //   // physics: NeverScrollableScrollPhysics(),
+      //   child: Container(
+      //     color: Colors.yellow,
+      //     // Column - 세로는 부모의 Maximum - 즉 전체 화면의 세로 사이즈 쓰는 중. 가로는 자식 중 가장 큰놈
+      //     // 현재 Column의 사이즈: 가로는 딱 자식 요소 만큼만인 상태, 세로는 전체
+      //     child: Column(
+      //       children: <int>[1, 2, 3, 4].map<Widget>(
+      //           (int i) => Row(
+      //             children: <Map<String, dynamic>>[
+      //               {
+      //                 "imgPath": " ",
+      //                 "name": "",
+      //                 "isActive": true
+      //               },
+      //               {
+      //                 "imgPath": " ",
+      //                 "name": "",
+      //                 "isActive": false
+      //               }
+      //             ].map<Widget>(
+      //                 (Map<String, dynamic> m) => My.MyWidget(
+      //                   imgPath: m['imgPath'],
+      //                   name: m['name'],
+      //                   isActive: m['isActive']
+      //                 )
+      //             ).toList(),
+      //           )
+      //       ).toList(),
+      //       // mainAxisAlignment: MainAxisAlignment.center,
+      //       // crossAxisAlignment: CrossAxisAlignment.start,
+      //       // children: [
+      //       //   Container(
+      //       //     // width: MediaQuery.of(context).size.width * 0.5,
+      //       //     color: Colors.red,
+      //       //     child: Row(
+      //       //       children: [
+      //       //         My.MyWidget(imgPath: "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg", name: "name1", isActive: true),
+      //       //         // this._myWidget(context: context, imgPath: "https://cdn.pixabay.com/photo/2022/09/03/10/52/northern-gannets-7429282_1280.jpg", name: "name2", isActive: false),
+      //       //         My.MyWidget(
+      //       //             imgPath: "https://cdn.pixabay.com/photo/2022/08/20/22/25/sea-7400124_640.jpg",
+      //       //             name: "Hi",
+      //       //             isActive: true
+      //       //         )
+      //       //       ],
+      //       //
+      //       //     ),
+      //       //   ),
+      //       //
+      //       // ],
+      //     )
+      //   ),
+      // )
+
     );
   }
-
-  // Private 하게 _로 만들기
-  // Widget _myWidget({required BuildContext context, required String imgPath, required String name, required bool isActive}){
-  //   return Container(
-  //     // padding: EdgeInsets.only(right:10.0),
-  //     // alignment: Alignment.bottomRight,
-  //     // Context는 build에 들어던 것.
-  //     width: MediaQuery.of(context).size.width * 0.5,
-  //     color: Colors.white,
-  //     child: Column(
-  //       children: [
-  //         Container(
-  //             alignment: Alignment.bottomRight,
-  //             color: Colors.greenAccent,
-  //             padding: EdgeInsets.only(right:10.0),
-  //             child: Icon(Icons.more_horiz)
-  //         ),
-  //         // Stack을 사용했을 때, Stack의 Children이 Layer처럼 쌓인다.
-  //         // Positioned를 사용하는 기본이다. Positioned를 사용하지 않으면, 막쌓이는 것.
-  //         // Stack의 범위가 가장 큰 것을 기준으로 Positioned가 된다.
-  //         // Stack 쌓일때는, 무엇이 가장 큰것인지 확인하고, 그 상태에서 포지션을 정해진다.
-  //         // 기준으로 잡을 곳에만 Stack으로 쌓이면 된다.
-  //         Stack(
-  //           // Stack 내부 요소들이 가운데로 오도록 만든 것.
-  //             alignment: AlignmentDirectional.center,
-  //             // fit: StackFit.passthrough,
-  //             children: [
-  //               Container(
-  //                 margin: EdgeInsets.symmetric(vertical: 10.0),
-  //                 width : 100.0,
-  //                 height: 100.0,
-  //                 // child: Image.network("https://cdn.pixabay.com/photo/2022/04/16/01/36/woman-7135489_1280.jpg")
-  //                 decoration: BoxDecoration(
-  //                   image: DecorationImage(
-  //                       image: NetworkImage(imgPath),
-  //                       fit: BoxFit.cover
-  //                   ),
-  //                   borderRadius: BorderRadius.circular(100.0),
-  //                   color: Colors.purple,
-  //                 ),
-  //               ),
-  //
-  //               // isActive가 True면 보여주고, False면 안보여진다.
-  //               // 근데 Chilren의 요소이다. list의 요소이다.
-  //               // 삼항연산자가 존재한다.
-  //               isActive ? Positioned(
-  //                   top: 8.0,
-  //                   right: 10.0,
-  //                   child: Container(
-  //                     width: 20.0,
-  //                     height: 20.0,
-  //                     decoration: BoxDecoration(
-  //                         borderRadius: BorderRadius.circular(30.0),
-  //                         color: Colors.black,
-  //                         border: Border.all(
-  //                           color: Colors.white,
-  //                           width: 4.0,
-  //                         )
-  //                     ),
-  //                   )
-  //
-  //               ) : Container(
-  //                 // Container의 사이즈가 Stack이 사이즈인데, Stack의 사이즈가 Container의 사이즈가 동일했기 때문에 문제가 없었는데
-  //                 // 비어있는 Container가 들어간 이후로부터는,
-  //                 // StackFit이 Loose로 되어있다. 그거때문에, 제어가 안되는 애들은 그냥 꽉 차버리는 것. mainAxisSize.max가 되는 것.
-  //                 // color: Colors.purple,
-  //                 // height: 20.0,
-  //               )
-  //               // 아래처럼 한겹 더 있는데 안보이게도 가능함.
-  //               // Positioned(
-  //               //   left:0,
-  //               //   right:0,
-  //               //   top: 0,
-  //               //   bottom: 0,
-  //               //   child: Container()
-  //               // )
-  //             ]
-  //         ),
-  //         // fontSize의 기준은 무엇일까? 14bp 로 내부에서 설정
-  //         //
-  //         Container(
-  //           color: Colors.blue,
-  //           // alignment: Alignment.bottomRight,
-  //           child: Text("Name : $name", style: TextStyle(
-  //             fontSize: 16.0,
-  //             fontWeight: FontWeight.bold,
-  //           ),),
-  //         ),
-  //         Container(
-  //             padding: EdgeInsets.symmetric(vertical: 20.0),
-  //             // color: Colors.purple,
-  //             // 균등하게 분할하고 싶을 때,
-  //             child: Row(
-  //               // Row가 Evenly라고 하면, 띄어져 있는 곳들을 다 균등하게.
-  //               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //               children: [
-  //                 Icon(Icons.person),
-  //                 Icon(Icons.add_a_photo)
-  //               ],
-  //             )
-  //         )
-  //       ],
-  //     ),
-  //   );
-  // }
 }
-
-
-
-
-
-
-
---
+++ lib/pages/detailPage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: IconButton(
+        icon: Icon(Icons.navigate_next),
+        onPressed: (){
+          // 리스트의 마지막 pop하고 push해주는 것.
+          // Inspector에서 확인해야 한다.
+          // Flutter에서 기본으로 제공하는 뒤로가기 누르면, 현재페이지 날리고 뒤로가기.
+
+          // 현재 자리 대체하는 것. 중복으로도 계속 기록되는 것. 중복이여도 계속 쌓이는 것.
+          // pushReplacement를 하면, 지금꺼 지우고 넣는다.
+          Navigator.of(context).push<dynamic>(
+            MaterialPageRoute(builder: (BuildContext _){
+              // return MainPage()
+              // return DetailPage()
+              return Scaffold(
+                appBar: AppBar(),
+              );
+            })
+          );
+        },
+      )
+    );
+  }
+}
+
+
