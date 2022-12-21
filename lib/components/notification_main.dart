730dd2bea8ec6b7893f8d2fb4d6e59f3d9afa652
saanghyuk <saanghyuk@gmail.com>
2022-12-21 19:56:14 +0900

    20221221

---
+++ lib/components/notification_main.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyRoute());
+
+class MyRoute extends StatefulWidget {
+  const MyRoute({Key? key}) : super(key: key);
+
+  @override
+  State<MyRoute> createState() => _MyRouteState();
+}
+
+class _MyRouteState extends State<MyRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<User>(
+        onNotification: (User user){
+          setState((){});
+          return true;
+        },
+        child: NotificationListener<Counter>(
+            onNotification: (Counter state){
+              setState(() {});
+              return true;
+            },
+            child : MaterialApp(
+              onGenerateRoute: (RouteSettings route){
+                return MaterialPageRoute(
+                    settings: RouteSettings (name : MainPage.path),
+                    builder : (BuildContext context) => MainPage()
+                );
+              },
+            )
+        ));
+  }
+}
+
+
+class MainPage extends StatelessWidget {
+  static const String path = "/main";
+  // 생성된 객체를 가져다 쓰기 위함
+  final Counter _counter = Counter.state;
+  // const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("Main Page");
+    return Scaffold(
+      appBar: AppBar(
+          actions : [
+            IconButton(
+                onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => DetailPage())),
+                icon: Icon(Icons.skip_next))
+          ]
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(this._counter.value.toString()),
+              Text(User.state.name.toString())
+            ],
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {
+          this._counter.add(context)
+        },
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final Counter _counter = Counter.state;
+  final User _user = User.state;
+
+  // const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(backgroundColor: Colors.green,),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              children: [
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 20.0),
+                    decoration: BoxDecoration(border:Border.all()),
+                    child: Column(
+                      children: [
+                        Center(child: Text("counter : ${this._counter.value}")),
+                        IconButton(
+                            onPressed: () => this._counter.add(context),
+                            icon: Icon(Icons.add))
+                      ],
+                    )
+                ),
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 20.0),
+                    decoration: BoxDecoration(border: Border.all()),
+                    child: Column(
+                      children: [
+                        Text("Username : ${this._user.name ?? "Empty"}"),
+                        SizedBox(height: 20.0),
+                        Container(
+                            child: TextField(
+                              decoration: InputDecoration(filled: true),
+                              onSubmitted: (String inputTxt) => this._user.setName(v: inputTxt, context: context),
+                            )
+                        )
+                      ],
+                    )
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+
+
+class Counter extends Notification{
+  // 외부에서 instantate하지 말고 이거 가져다 쓰라는 뜻
+  static Counter state = Counter._instance();
+  Counter._instance();
+  int value = 0;
+  void add(BuildContext context){
+    this.value++;
+    this.dispatch(context);
+  }
+}
+
+class User extends Notification{
+  // 외부에서 instantate하지 말고 이거 가져다 쓰라는 뜻
+  static User state = User._instance();
+  User._instance();
+  String? _name;
+  String? get name => this._name;
+  void setName({required String v, required BuildContext context}){
+    this._name = v;
+    this.dispatch(context);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,148 +1,87 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; 
 
-void main() => runApp(MyRoute()); 
+void main() => runApp(MyRoute());
 
-class MyRoute extends StatefulWidget {
+class MyRoute extends StatelessWidget {
   const MyRoute({Key? key}) : super(key: key);
 
   @override
-  State<MyRoute> createState() => _MyRouteState();
-}
-
-class _MyRouteState extends State<MyRoute> {
-  @override
   Widget build(BuildContext context) {
-    return NotificationListener<User>(
-        onNotification: (User user){
-          setState((){});
-          return true;
-        },
-        child: NotificationListener<Counter>(
-          onNotification: (Counter state){
-            setState(() {});
-            return true;
-          },
-          child : MaterialApp(
-            onGenerateRoute: (RouteSettings route){
-              return MaterialPageRoute(
-                settings: RouteSettings (name : MainPage.path),
-                builder : (BuildContext context) => MainPage()
-              );
-            },
-          )
-        ));
-  }
-}
-
-
-class MainPage extends StatelessWidget {
-  static const String path = "/main";
-  // 생성된 객체를 가져다 쓰기 위함
-  final Counter _counter = Counter.state;
-  // const MainPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    print("Main Page");
-    return Scaffold(
-      appBar: AppBar(
-        actions : [
-          IconButton(
-              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => DetailPage())),
-              icon: Icon(Icons.skip_next))
-        ]
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(this._counter.value.toString()),
-            Text(User.state.name.toString())
-          ],
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => {
-          this._counter.add(context)
-        },
-      ),
-    );
-  }
-}
-
-class DetailPage extends StatelessWidget {
-  final Counter _counter = Counter.state;
-  final User _user = User.state;
-
-  // const DetailPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.green,),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            children: [
-              Container(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  decoration: BoxDecoration(border:Border.all()),
-                  child: Column(
-                    children: [
-                      Center(child: Text("counter : ${this._counter.value}")),
-                      IconButton(
-                          onPressed: () => this._counter.add(context),
-                          icon: Icon(Icons.add))
-                    ],
-                  )
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 20.0),
-                decoration: BoxDecoration(border: Border.all()),
-                child: Column(
-                  children: [
-                    Text("Username : ${this._user.name ?? "Empty"}"),
-                    SizedBox(height: 20.0),
-                    Container(
-                      child: TextField(
-                        decoration: InputDecoration(filled: true),
-                        onSubmitted: (String inputTxt) => this._user.setName(v: inputTxt, context: context),
-                      )
-                    )
-                  ],
-                )
-              )
-            ],
+    // return ChangeNotifierProvider<MyState>(
+    //   create : (BuildContext context) => MyState(),
+    //   child: MaterialApp(
+    //     home: ProviderSamplePage(),
+    //   ),
+    // );
+    print("ROUTE");
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<MyState>(
+              create: (BuildContext context) => MyState()
+            // Child는 입력 안함
           ),
-        ),
+          ChangeNotifierProvider<MyState2>(
+              create: (BuildContext context) => MyState2()
+            // Child는 입력 안함
+          )
+        ],
+      child: MaterialApp(
+        home : ProviderSamplePage()
       )
     );
   }
 }
 
+class ProviderSamplePage extends StatelessWidget {
+  const ProviderSamplePage({Key? key}) : super(key: key);
 
 
-class Counter extends Notification{
-  // 외부에서 instantate하지 말고 이거 가져다 쓰라는 뜻
-  static Counter state = Counter._instance(); 
-  Counter._instance(); 
-  int value = 0; 
-  void add(BuildContext context){
-    this.value++; 
-    this.dispatch(context);
+  @override
+  Widget build(BuildContext context) {
+    print("PAGE");
+    // MyState state = Provider.of<MyState>(context, listen:false);
+    // MyState2 state2 = Provider.of<MyState2>(context);
+    MyState state = context.read<MyState>();
+    MyState2 state2 =  context.watch<MyState2>();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text(state.v.toString())
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: state.add,
+      ),
+    );
   }
 }
 
-class User extends Notification{
-  // 외부에서 instantate하지 말고 이거 가져다 쓰라는 뜻
-  static User state = User._instance();
-  User._instance();
-  String? _name;
-  String? get name => this._name; 
-  void setName({required String v, required BuildContext context}){
-    this._name = v; 
-    this.dispatch(context);
+
+class MyState extends ChangeNotifier{
+  MyState(){
+    print("MyState is built");
+  }
+  int v = 0;
+  void add() {
+    this.v += 1;
+    this.notifyListeners(); // 상태를 변경했을 때만 붙이면 된다.
   }
 }
+
+
+class MyState2 extends ChangeNotifier{
+  MyState(){
+    print("MyState is built");
+  }
+  int v = 0;
+  void add() {
+    this.v += 1;
+    print(this.v);
+    this.notifyListeners(); // 상태를 변경했을 때만 붙이면 된다.
+  }
+}
+
+
+
+
