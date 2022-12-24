ee0143cb4addeb87f253efdc64d8b234d4adf1b7
saanghyuk <saanghyuk@gmail.com>
2022-12-24 15:03:11 +0900

    review 20221224

--- lib/components/provider.dart
+++ lib/components/provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; 
- 
-void main() => runApp(MyRoute()); 
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyRoute());
 
 class MyRoute extends StatelessWidget {
   const MyRoute({Key? key}) : super(key: key);
@@ -9,20 +9,26 @@ class MyRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return ChangeNotifierProvider<MyState>(
-    //   create: (BuildContext context) => MyState(),
+    //   create : (BuildContext context) => MyState(),
     //   child: MaterialApp(
     //     home: ProviderSamplePage(),
-    //
     //   ),
     // );
+    print("ROUTE");
     return MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (BuildContext context) => MyState()),
-        ChangeNotifierProvider(create: (BuildContext context) => MyState2()),
-    ],
-    child: MaterialApp(
-      home: ProviderSamplePage()
-      )
+          ChangeNotifierProvider<MyState>(
+              create: (BuildContext context) => MyState()
+            // Child는 입력 안함
+          ),
+          ChangeNotifierProvider<MyState2>(
+              create: (BuildContext context) => MyState2()
+            // Child는 입력 안함
+          )
+        ],
+        child: MaterialApp(
+            home : ProviderSamplePage()
+        )
     );
   }
 }
@@ -34,15 +40,14 @@ class ProviderSamplePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("PAGE");
-    // MyState state = Provider.of<MyState>(context);
+    // MyState state = Provider.of<MyState>(context, listen:false);
     // MyState2 state2 = Provider.of<MyState2>(context);
-    MyState state = context.watch<MyState>();
+    MyState state = context.read<MyState>();
     MyState2 state2 =  context.watch<MyState2>();
-
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text(state.v.toString())
+          child: Text(state.v.toString())
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -55,17 +60,6 @@ class ProviderSamplePage extends StatelessWidget {
 
 class MyState extends ChangeNotifier{
   MyState(){
-    print("MyState is build!");
-  }
-  int v = 0;
-  void add() {
-    this.v += 1;
-    this.notifyListeners();
-  }
-}
-
-class MyState2 extends ChangeNotifier{
-  MyState(){
     print("MyState is built");
   }
   int v = 0;
@@ -74,3 +68,20 @@ class MyState2 extends ChangeNotifier{
     this.notifyListeners(); // 상태를 변경했을 때만 붙이면 된다.
   }
 }
+
+
+class MyState2 extends ChangeNotifier{
+  MyState(){
+    print("MyState is built");
+  }
+  int v = 0;
+  void add() {
+    this.v += 1;
+    print(this.v);
+    this.notifyListeners(); // 상태를 변경했을 때만 붙이면 된다.
+  }
+}
+
+
+
+
--- lib/components/single_provider.dart
+++ lib/components/single_provider.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; 
- 
-void main() => runApp(MyRoute()); 
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyRoute());
 
 class MyRoute extends StatelessWidget {
   const MyRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // return ChangeNotifierProvider<MyState>(
-    //   create: (BuildContext context) => MyState(),
-    //   child: MaterialApp(
-    //     home: ProviderSamplePage(),
-    //
-    //   ),
-    // );
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (BuildContext context) => MyState()),
-        ChangeNotifierProvider(create: (BuildContext context) => MyState2()),
-    ],
-    child: MaterialApp(
-      home: ProviderSamplePage()
-      )
+    return ChangeNotifierProvider<MyState>(
+      create: (BuildContext context) => MyState(),
+      child: MaterialApp(
+        home: ProviderSamplePage(),
+
+      ),
     );
   }
 }
@@ -30,19 +21,14 @@ class MyRoute extends StatelessWidget {
 class ProviderSamplePage extends StatelessWidget {
   const ProviderSamplePage({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     print("PAGE");
-    // MyState state = Provider.of<MyState>(context);
-    // MyState2 state2 = Provider.of<MyState2>(context);
-    MyState state = context.watch<MyState>();
-    MyState2 state2 =  context.watch<MyState2>();
-
+    MyState state = Provider.of<MyState>(context);
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text(state.v.toString())
+          child: Text(state.v.toString())
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
