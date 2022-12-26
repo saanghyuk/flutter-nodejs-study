f1a29e59b8f6a17aa3e940b88395519941bb0756
saanghyuk <saanghyuk@gmail.com>
2022-12-26 17:51:35 +0900

    20221226 review

--- lib/main.dart
+++ lib/main.dart
@@ -8,85 +8,125 @@ class MyRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // return ChangeNotifierProvider<MyState>(
+    //   create: (BuildContext context) => MyState(),
+    //   child: MaterialApp(
+    //     home: ProviderSamplePage(),
+    //
+    //   ),
+    // );
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (BuildContext context) => MyState()),
-        ChangeNotifierProvider(create: (BuildContext context) => MyState2())
-      ],
-      child: MaterialApp(
-        home: ProviderSamplePage(),
-      ),
+        providers: [
+          ChangeNotifierProvider(create: (BuildContext context) => MyState()),
+          ChangeNotifierProvider(create: (BuildContext context) => MyState2()),
+        ],
+        child: MaterialApp(
+            // home: ChangeNotifierProvider<MyState>(
+            //     create: (BuildContext context) => MyState(),
+            //     child: ProviderSamplePage()
+            // )
+          home: ProviderSamplePage(),
+        )
     );
   }
 }
-
-
 
 class ProviderSamplePage extends StatelessWidget {
   const ProviderSamplePage({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    MyState2 state2 = context.watch<MyState2>();
+    print("PAGE");
+    // MyState state = Provider.of<MyState>(context);
+    // MyState2 state2 = Provider.of<MyState2>(context);
+    MyState state = context.watch<MyState>();
+    MyState2 state2 =  context.watch<MyState2>();
+    // return ProviderSampleView(viewTxt: state.v.toString(), onPressed: (){
+    //   state.add();
+    //   Navigator.of(context).push(
+    //       MaterialPageRoute(
+    //         builder: (_) => ProviderSamplePage2(),
+    //       )
+    //   );
+    // });
+    return ProviderSampleView(
+      viewObj: state.state((){
+              Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (_) => ProviderSamplePage2(),
+                    )
+                  );
+        })
+      );
+  }
+}
+
+
+class ProviderSampleView extends StatelessWidget {
+  // final String viewTxt;
+  // final void Function() onPressed;
+  // const ProviderSampleView({Key? key, required this.viewTxt, required this.onPressed}) : super(key: key);
+  final ViewObj viewObj;
+  const ProviderSampleView({required this.viewObj});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Text("MyState2"),
-          Text(state2.v.toString()),
-          IconButton(
-              onPressed: (){
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => ProviderSamplePage2()));
-              },
-              icon: Icon(Icons.navigate_next))
-        ]
+      body: Center(
+        child: Text(this.viewObj.data)
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: state2.add,
+        onPressed: this.viewObj.cb,
       ),
     );
   }
 }
 
+
 class ProviderSamplePage2 extends StatelessWidget {
   const ProviderSamplePage2({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    MyState2 state2 = Provider.of<MyState2>(context);
-    return Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            Text("State2"),
-            Text(state2.v.toString()),
-            IconButton(
-                onPressed: state2.add,
-                icon: Icon(Icons.add)
-            )
-          ],
-        )
-    );;
+    MyState state = context.watch<MyState>();
+    return ProviderSampleView(
+      viewObj: ViewObj(data: state.v.toString(), cb: state.add),
+    );
   }
+}
+
+
+class ViewObj{
+  final String data;
+  final void Function() cb;
+  const ViewObj({required this.data, required this.cb});
 }
 
 
 class MyState extends ChangeNotifier{
   MyState(){
-    print("MyState is built");
+    print("MyState is build!");
   }
   int v = 0;
   void add() {
     this.v += 1;
-    this.notifyListeners(); // 상태를 변경했을 때만 붙이면 된다.
+    this.notifyListeners();
+  }
+
+  ViewObj state(void Function() cb){
+    return ViewObj(data: this.v.toString(), cb: (){
+      this.add();
+      cb();
+  });
   }
 }
 
-
 class MyState2 extends ChangeNotifier{
   MyState(){
-    print("MyState2 is built");
+    print("MyState is built");
   }
   int v = 0;
   void add() {
