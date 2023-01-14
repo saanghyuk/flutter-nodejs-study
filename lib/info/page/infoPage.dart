6d6e1442f13d4bd21d84df562459d3c994f5483b
saanghyuk <saanghyuk@gmail.com>
2023-01-14 12:58:28 +0900

    review before class

--- lib/info/page/infoPage.dart
+++ lib/info/page/infoPage.dart
@@ -14,18 +14,14 @@ class InfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     InfoProvider provider = context.watch<InfoProvider>();
-
     if(provider.state == null) return LoadingPage();
-    /// @TODO: 상태 주입
     return InfoPageView(
       itemCount: provider.state!.length,
       data: provider.state!,
+      onTap: provider.setIndex,
 
-      // provider.setIndex <<- 그냥 이렇게 줄일 수 있다.
-      onTap: (int index){
-        provider.setIndex(index);
-      }
     );
+
   }
 }
 
@@ -40,22 +36,22 @@ class InfoPageView extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: (){
-            Navigator.of(context).pushNamed(InfoPage.path);
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: (){
+                Navigator.of(context).pushNamed(InfoPage.path);
               },
-            icon: Icon(Icons.add),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GridTitleWidget(
+              icon: Icon(Icons.add),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: GridTitleWidget(
                 title: Container(
-                  child: Text("Welcome!", style: TextStyle(fontSize: 25),)
+                    child: Text("Welcome!", style: TextStyle(fontSize: 25),)
                 ),
                 padding: EdgeInsets.all(20.0),
                 delegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -81,17 +77,13 @@ class InfoPageView extends StatelessWidget {
                       Icon(Icons.settings)
                     ],
                     onTap: () async {
-                      // 화면 움직이는 것 자체는 이 뷰에서 그대로 가지고 있어야 한다.
-                      // Navigator 위 아래 중 하나에서만 동작하면 된다.
                       this.onTap(index);
                       await Navigator.of(context).pushNamed(InfoDetailPage.path);
-                      // this.onTapAfter();
                     }),
-            ),
-          )
-        ],
-      )
+              ),
+            )
+          ],
+        )
     );
   }
 }
-
--- lib/providers/infoProvider.dart
+++ lib/providers/infoProvider.dart
@@ -1,47 +1,35 @@
 import 'package:flutter/foundation.dart';
 
 //
-class InfoProvider with ChangeNotifier{
+class InfoProvider with ChangeNotifier {
   List<DataModel>? state;
-  // 현재 어디를 클릭한 상태인건지를 알아야 함.
-  // state처럼 Public하게 됨. 뒤에서, provider.state = [] 이렇게 바꿔버릴 수가 있음.
-  // setter를 동작하지 못하게
+
   int? _selectIndex;
-  int? get selectIndex  => this.selectIndex;
-  // setter, _selectIndex가 nullable하니깐, 여기서의 매개변수도 nullable하게
-  // provider.selectIndex = v인 것. 대입연산자에 들어간 것을 함수의 파라미터로
+  int? get selectIndex => this._selectIndex;
+
   // set selectIndex(int? v){
   //   this._selectIndex = v;
   // }
 
   void setIndex(int? index){
     this._selectIndex = index;
-    // Provider에서 언제 notifyListner를 쓸 것인가?
-    // 값을 바꿨을 때, 그냥 하는 것. 화면에서 당장 바뀔 것이 없어도.
-    // View의 상태.
-    // 상태로 취급할 변수가 바뀌면 그냥 notifyListner하는 것.
-    // int _d = 0; 처럼 바뀌든 말든 View랑 아무상관 없는 것은 notifyLinstner를 안써도 되지만,
-    // 상태와 관련된 뭔가를 할려면, notifyLinstner를 하는 것.
     this.notifyListeners();
   }
 
-  InfoProvider(){
-    Future.delayed(Duration(seconds:3 ), this.init);
+  InfoProvider() {
+    Future.delayed(Duration(seconds: 3), this.init);
   }
 
-
-
-
-  void init(){
+  void init() {
     this.state = this.fetch();
     this.notifyListeners();
   }
 
-  List<DataModel> fetch(){
+  List<DataModel> fetch() {
     List _dummy = [{
       "title": "edm",
-      "titleSrc": "",
-      "des" : [
+      "titleSrc": "https://ssl.pstatic.net/melona/libs/1431/1431651/de4b29fe8dd5a280c53e_20221227144124138.jpg",
+      "des": [
         {
           "title": "0",
           "des": "0",
@@ -52,45 +40,32 @@ class InfoProvider with ChangeNotifier{
     return this.parse(_dummy);
   }
 
-  List<DataModel> parse(List json){
-    return json.map<DataModel>((dynamic e){
+  List<DataModel> parse(List json) {
+    return json.map<DataModel>((dynamic e) {
       // e를 넘겨서 알아서 생성하도록 하자.
       return DataModel<ItemModel>.fromJson(
-          e,
-          (List des){
-            return des.map<ItemModel>((t) => ItemModel.fromJson(t)).toList();
+              e,
+              (List des){
+                return des.map<ItemModel>((dynamic t) =>
+                  ItemModel.fromJson(t)).toList();
           },
-      // return DataModel(
-      //     title: e['title'].toString(),
-      //     // map의 반환값은 iterable이다. 그래서, List로 만들어 준 것.
-      //     des: List.of(e['des']).map<ItemModel>((t) {
-      //       return ItemModel(
-      //         title: t['title'].toString(),
-      //         des: t['des'].toString(),
-      //         src: t['src'].toString(),
-      //       );
-      //     }).toList(),
       );
     }).toList();
   }
 }
 
 
-class DataModel<E>{
+class DataModel<E> {
   final String title;
   final String titleSrc;
   final List<E> des;
-  const DataModel({required this.title, required this.des, required this.titleSrc});
+  const DataModel({required this.title, required this.titleSrc, required this.des});
 
   // e의 value 제네릭은 dynamic으로 될 수 밖에 없다.
   DataModel.fromJson(Map<String, dynamic> e, List<E> Function(List) c)
       : title = e['title'],
-        titleSrc = e['titleSrc'].toString(),
-        // t는 MAP인데 map인것을 모른다. e에 들어있는 des가 List인 것도 모른다.
-        // 왜냐면, e를 보면, key는 String이지만, 값은 dynamic이라고 해놨다.
-        // itemModel에 넘기는 것도 dyamic이다.
+        titleSrc = e['titleSrc'],
         des = c(List.of(e['des']));
-        // des = List.of(e['des']).map<ItemModel>((dynamic t) => ItemModel.fromJson(t)).toList();
 }
 
 class ItemModel{
@@ -108,5 +83,4 @@ class ItemModel{
       : title=t['title'].toString(),
         des = t['des'].toString(),
         src = t['src'].toString();
-
 }
